cd5884d592587eba6d1403ada96684803b7431c2
supun_m <a.m.s.madhuranga@gmail.com>
2026-03-05 09:06:13 +0530

    Course.java class created

---
+++ src/main/java/com/epic/studentmanagementsystemepicdemo/model/Course.java
@@ -0,0 +1,13 @@
+package com.epic.studentmanagementsystemepicdemo.model;
+
+import lombok.Data;
+
+@Data
+public class Course {
+
+    private Integer id;
+    private String courseName;
+    private String courseCode;
+    private String description;
+
+}
